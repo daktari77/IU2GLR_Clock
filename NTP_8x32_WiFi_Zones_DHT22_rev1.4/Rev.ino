41a37481007d5153b126f50a95f9e96ce122a489
Michele <michele.martino77@gmail.com>
2022-09-11 13:18:15 +0200

    Add files via upload

---
+++ NTP_8x32_WiFi_Zones_DHT22_rev1.4/Rev.ino
@@ -0,0 +1,27 @@
+const char rev = 1.4;
+/*
+   rev 1.4
+   07 settembre 2022
+   visualizzazione alternata data / temp + umidità
+   08 settembre 2022
+   Semplificazione script
+*/
+/*
+   05 settembre 2022
+   ridotto temp ad un sola cifra decimale
+   aggiunto simbolo "°c" alla temepratura
+   aggiunto 8x32 led per un totale di 24 moduli
+*/
+/*
+   03/settembre2022
+   Aggiunto display umudità
+*/
+/*
+   01/settembre/2022
+   agguinta libreria DHTesp per sensore DHT11/22
+*/
+/*
+   31/agosto/2022
+   ottimizzazione memoria rimuovendo animazioni non necessarie dal file:
+   C:\Users\marmi01\OneDrive\Documenti\Arduino\libraries\arduino_667928\src\MD_Parola.h
+*/
---
+++ NTP_8x32_WiFi_Zones_DHT22_rev1.4/secret.h
@@ -0,0 +1,6 @@
+//__________________________________________________your wifi settings
+const char* ssid       = "MARTINET-OUT";
+const char* password   = "asdfghjkl";
+//const char* ssid       = "HUAWEI Mate 8";
+//const char* password   = "1cc8c848bf55";
+//___________________________________________________
