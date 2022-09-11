129f9b51ed61b2d5a10d72a3973a4d163cc9740c
Michele <michele.martino77@gmail.com>
2022-09-12 00:56:22 +0200

    Update secret.h

--- NTP_8x32_WiFi_Zones_DHT22_rev1.5/secret.h
+++ NTP_8x32_WiFi_Zones_DHT22_rev1.5/secret.h
@@ -1,6 +1,4 @@
 //__________________________________________________your wifi settings
-const char* ssid       = "MARTINET-OUT";
-const char* password   = "asdfghjkl";
-//const char* ssid       = "HUAWEI Mate 8";
-//const char* password   = "1cc8c848bf55";
+const char* ssid       = "MYSSID";
+const char* password   = "MYPASSWORD";
 //___________________________________________________
