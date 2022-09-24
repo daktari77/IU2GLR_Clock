ebf53ec81bac97da3ebda4a24e8562e571817ca3
Michele <michele.martino77@gmail.com>
2022-09-24 23:06:35 +0200

    Update secret.h

--- DHT22_NTP_MQTT_WiFi_TimeZone_Zones_rev2.0/secret.h
+++ DHT22_NTP_MQTT_WiFi_TimeZone_Zones_rev2.0/secret.h
@@ -1,6 +1,4 @@
 //__________________________________________________your wifi settings
-const char* WIFI_SSID       = "MARTINET-OUT";
-const char* WIFI_PASSWORD   = "asdfghjkl";
-//const char* ssid       = "HUAWEI Mate 8";
-//const char* password   = "1cc8c848bf55";
+const char* WIFI_SSID       = "SSID";
+const char* WIFI_PASSWORD   = "PASSWORD";
 //___________________________________________________
