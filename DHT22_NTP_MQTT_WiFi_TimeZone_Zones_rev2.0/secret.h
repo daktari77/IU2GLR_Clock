aafbe905c88186bdd5232ae4f75b5c177716b2b5
Michele <michele.martino77@gmail.com>
2022-09-24 23:05:50 +0200

    Add files via upload

---
+++ DHT22_NTP_MQTT_WiFi_TimeZone_Zones_rev2.0/secret.h
@@ -0,0 +1,6 @@
+//__________________________________________________your wifi settings
+const char* WIFI_SSID       = "MARTINET-OUT";
+const char* WIFI_PASSWORD   = "asdfghjkl";
+//const char* ssid       = "HUAWEI Mate 8";
+//const char* password   = "1cc8c848bf55";
+//___________________________________________________
