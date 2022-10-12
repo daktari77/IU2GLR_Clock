7e9018376092e7cd3f86c010cc14e18dd0fb1a34
Michele <michele.martino77@gmail.com>
2022-10-12 13:01:45 +0200

    Update secret.h

--- WALL_Clock_rev2.0b.01/secret.h
+++ WALL_Clock_rev2.0b.01/secret.h
@@ -1,13 +1,4 @@
 //__________________________________________________your wifi settings
-//const char* WIFI_SSID       = "MARTINET-OUT";
-//const char* WIFI_PASSWORD   = "asdfghjkl";
-const char* WIFI_SSID = "MARTINET-IN";
-const char* WIFI_PASSWORD = "ASDFGHJKL";
-//const char* WIFI_SSID       = "HUAWEI Mate 8";
-//const char* WIFI_PASSWORD   = "1cc8c848bf55";
-
-const char* MQTT_UID = "MQTTuser";
-const char* MQTT_PWD = "ttLAHkg=7cakEn";
-
-
+//const char* WIFI_SSID       = "MY-SSID";
+//const char* WIFI_PASSWORD   = "MY-PWD";
 //___________________________________________________
